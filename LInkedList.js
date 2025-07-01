cc807bdf1d6ba8f9f5a9fc864872cffbafaa9afd
Rubai Rahman <rubairahman1@gmail.com>
2025-07-01 09:37:27 +0600

    push method done

--- LInkedList.js
+++ LInkedList.js
@@ -14,10 +14,21 @@ class LinkedList {
     this.length = 1;
   }
 
-  
+  //push method
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
-
-
-
 let myLinkedList = new LinkedList(4);
+myLinkedList.push(7);
