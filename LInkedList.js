025637a60484e85055d156e02446490d493b75ba
Rubai Rahman <rubairahman1@gmail.com>
2025-07-01 22:17:57 +0600

    pop method added

--- LInkedList.js
+++ LInkedList.js
@@ -28,6 +28,31 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  //pop method
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+    if (!this.head.next) {
+      const temp = this.head;
+      this.head = null;
+      this.length--;
+      return temp;
+    }
+    else {
+      let temp = this.head;
+      let pre = null;
+      while (temp.next) {
+        pre = temp;
+        temp = temp.next;
+      }
+      this.tail = pre;
+      this.tail.next = null;
+      this.length--;
+      return temp;
+    }
+  }
 }
 
 let myLinkedList = new LinkedList(4);
